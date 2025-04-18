5159f382597751507cd5093f555bd7098aa2c30f
MinhP <benedict.phan@gmail.com>
2025-04-18 13:42:59 -0700

    build script update. added versioning, checks

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #/bin/bash
-pandoc --pdf-engine=wkhtmltopdf -s -c github-markdown.css -f gfm resume2025.md -o resume2025.pdf \
+OUTPUT="resume-$(date -I)"
+
+if [ ! -d "./artifacts" ]; then
+	mkdir ./artifacts
+fi
+
+if [ -f "./artifacts/$OUTPUT.pdf" ]; then
+	i=1
+	while [[ -f "./artifacts/${OUTPUT}_${i}.pdf" ]]; do
+		let i++
+	done
+	OUTPUT="resume-$(date -I)_$i"
+fi
+
+pandoc --pdf-engine=wkhtmltopdf -s -c github-markdown.css -f gfm resume.md -o ./artifacts/$OUTPUT.pdf \
     -V margin-top=10 \
     -V margin-left=10 \
     -V margin-right=10 \
     -V margin-bottom=10
+
+echo "Generated $OUTPUT.pdf"
